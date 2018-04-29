bbc405fbdf2945f4fdaf48679df66f243d647dde
miguelcecci <miguelnetojet@hotmail.com>
2018-04-29 00:58:23 -0300

    muitas coisas adicionadas

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -11,7 +11,7 @@ router.post('/', (req, res, next) => {
     const order = {
         productId: req.body.productId,
         quantity: req.body.quantity
-    }
+    };
     res.status(201).json({
         message: 'Orders was created',
         order: order
--- app.js
+++ app.js
@@ -2,22 +2,27 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb://admin:password@127.0.0.1:27017/test');
+const db = mongoose.connection;
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use((req, res, next) =>{
-    res.header('Access-Control-Allow-Origin', '*'):
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    if(req.method === 'OPTIONS'){
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({})
-    }
-});
+// app.use((req, res, next) =>{
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//     if(req.method === 'OPTIONS'){
+//         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+//         return res.status(200).json({})
+//     }
+//     next();
+// });
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
